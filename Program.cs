b7c4cf5335a41c24e2c40cf962e7fa895187efb6
ahmad jamal <ahmadjamalkhater@gmail.com>
2023-07-10 17:26:40 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab9_LINQInManhattan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
